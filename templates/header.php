02f06120eda3889c4dfcfc6d4976be49257e2362
ansonkao <anson.kao@gmail.com>
2014-05-18 12:51:36 +0000

    - Refactored the fixed background image to be iOS compatible - Collapsed header nav menu for mobile portrait - Finalized header text + relative background-image positioning - Added responsive breakpoint changes

--- templates/header.php
+++ templates/header.php
@@ -5,12 +5,21 @@
         <h1 class="text-center">
           <a class="brand" href="<?php echo home_url('/') ?>"><?php bloginfo('name'); ?></a>
         </h1>
+        <div id="header-location" class="text-center">
+          <span class="glyphicon glyphicon-map-marker"></span> Toronto, ON, Canada
+        </div>
       </div>
     </div>
   </div>
   <div class="row">
     <div class="col-lg-12">
-      <nav class="nav-main text-center" role="navigation">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <nav class="collapse navbar-collapse nav-main text-center" role="navigation">
         <?php
           if (has_nav_menu('primary_navigation')) :
             wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-anson-toolbar'));
