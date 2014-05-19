04e607b25316d91e38ff92216fb909c913c5a402
ansonkao <anson.kao@gmail.com>
2014-05-19 19:02:41 +0000

    Favicon added

--- templates/head.php
+++ templates/head.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 
   <?php wp_head(); ?>
 
