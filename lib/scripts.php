64158b7e9ff870ff81c0e6db5cd3d10cd3cd5c5a
ansonkao <anson.kao@gmail.com>
2014-06-13 17:39:58 +0000

    Adjusted image alignment breakpoint for responsive.

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'baacc3541b2d755455cc4eb007cc3efe');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c868d00e13f0c7ab38558c7ae58be244');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
