a46e4f5b2e26107390fb0f69b498572d7f7abbf0
ansonkao <anson.kao@gmail.com>
2014-06-13 16:50:58 +0000

    Blockquotes now styled nicely.

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '57dc97dcf339c7c5019be5d08e343bef');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c79d8b232674469f597f22894a5d0e18');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
